1c7d885c515d74aedf7244c31949a13bb569f991
Кирилл Абрамов <work.kirillabramov@gmail.com>
2019-03-19 09:49:05 +0200

    Updates

---
+++ precache-manifest.a4eb9fcd669a9e7025459f8b01d19ad5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c86f624a4b91c70046f",
+    "url": "/animation/static/js/main.5c86f624.chunk.js"
+  },
+  {
+    "revision": "1a1b9554fd1312902f8c",
+    "url": "/animation/static/js/1.1a1b9554.chunk.js"
+  },
+  {
+    "revision": "0cfc31e770e6180dbcb3",
+    "url": "/animation/static/js/runtime~main.0cfc31e7.js"
+  },
+  {
+    "revision": "0fb0c4983dd680beffe43c831f18eb6f",
+    "url": "/animation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cdeea0978d8072bb799e4fc557ea21a.js"
+  "/animation/precache-manifest.a4eb9fcd669a9e7025459f8b01d19ad5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/animation/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
