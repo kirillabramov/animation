4825d8416aa896df8148be4b059630f942373d22
Кирилл Абрамов <work.kirillabramov@gmail.com>
2019-03-19 09:46:27 +0200

    Updates

---
+++ precache-manifest.3cdeea0978d8072bb799e4fc557ea21a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c86f624a4b91c70046f",
+    "url": "/static/js/main.5c86f624.chunk.js"
+  },
+  {
+    "revision": "1a1b9554fd1312902f8c",
+    "url": "/static/js/1.1a1b9554.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba9b886b01feca469886fe389e718c2a",
+    "url": "/index.html"
+  }
+];
